7319475c1d3f1919b828212c37d87406e5a653d5
Hamid <Bouayadihamid@gmail.com>
2023-09-12 13:10:07 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	return (0);
+}
