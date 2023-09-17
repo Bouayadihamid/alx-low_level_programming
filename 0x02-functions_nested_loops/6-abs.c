4d9282aaf15fe1d65f167767794a27a4b470562c
Hamid <Bouayadihamid@gmail.com>
2023-09-17 15:34:04 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _abs - return abs value
- * @n : number to check
- * Return:0 or 1
- */
+* _abs - return abs value
+* @n : number to check
+* Return:0 or 1
+*/
 
 int _abs(int n)
 {
-
-return (n >= 0 ? n : n * -1);
-
+	if (n >= 0)
+	{
+		return (1 * n);
+	}
+	else
+		return (n * -1);
+/* return (n >= 0 ? n : n * -1);*/
 }
-
