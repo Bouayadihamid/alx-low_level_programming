2231b87afe5ac03fe7f8c68f95db77b3581a344d
Hamid <Bouayadihamid@gmail.com>
2023-09-27 14:53:59 -0700

    Add untracked files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* more_numbers - print 10 times from 0 to 14.
+*
+* @a
+*
+* Return: print results
+*/
+
+void more_numbers(void)
+{
+	int a = 0;
+	int b;
+
+	while (a <= 9)
+	{
+		for (b = 0; b <= 14; b++) 
+		{	
+			if (b > 9)
+			{
+				_putchar('0' + b);
+			}
+			_putchar()
+		}
+		_putchar('\n');
+		a++;
+	}
+}
