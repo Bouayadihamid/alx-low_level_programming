6507a5477a7672d00d4d5954404e0e4efd11a1f3
Hamid <Bouayadihamid@gmail.com>
2023-10-02 14:10:53 -0700

    the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- is a function that counts
+ *
+ *@argc: integer
+ *@argv: character
+ *
+ *Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
