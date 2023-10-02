2db194c695b69e1558ff0e5560f8cf2d08fdacfb
Hamid <Bouayadihamid@gmail.com>
2023-10-02 13:59:33 -0700

    argv argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *-whatsmyname- is a function that prints the file name
+ *main- is a function that prints the file name
  *
  *@argc: integer
  *@argv: character
@@ -8,10 +8,10 @@
  *Return: return 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 
 {
 (void)argc;
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
