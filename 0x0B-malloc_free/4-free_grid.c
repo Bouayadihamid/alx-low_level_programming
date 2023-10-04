f7a4fd4baaac0a38d4ab05543ea0cb9e67feffa0
Hamid <Bouayadihamid@gmail.com>
2023-10-04 14:44:42 -0700

    fre_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *free_frid- is a function that frees a grid
+ *
+ *@grid: integer
+ *@height: integer
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+	free(grid[i]);
+	}
+free(grid);
+}
