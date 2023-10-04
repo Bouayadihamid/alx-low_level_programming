1bf485ff8c900afbc5598eeb24b969dd0b2eaa5d
Hamid <Bouayadihamid@gmail.com>
2023-10-03 19:38:37 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
+ *main- it the main functio
  *
+ *@argc: integer
+ *@argv: character
  *
- *
- *
- *
+ *Return: result
  */
 
 int main(int argc, char *argv[])
 {
-int i = 0;
+	int i, j, result;
 
-if (argv[] == " ")
-{
-	printf("%d\n", 0);
+	result = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				{
+					continue;
+				} else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	} else
+	printf("0\n");
+	return (0);
 }
-else if (argv[] < '48' || argv[] > '57')
-{
-	printf("Error\n");
-}
-for (i = 0; i < argv; i++)
-{
-	result = 
-	printf("%d\n", argv[i] + argv[argc - i]);
-}
-}
+
