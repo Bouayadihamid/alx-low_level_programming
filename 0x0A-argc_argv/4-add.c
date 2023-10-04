01c5bc900c2e38dfaa3a8bc9243f660ffbec4b51
Hamid <Bouayadihamid@gmail.com>
2023-10-03 19:22:07 -0700

    mul

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+
+if (argv[] == " ")
+{
+	printf("%d\n", 0);
+}
+else if (argv[] < '48' || argv[] > '57')
+{
+	printf("Error\n");
+}
+for (i = 0; i < argv; i++)
+{
+	result = 
+	printf("%d\n", argv[i] + argv[argc - i]);
+}
+}
