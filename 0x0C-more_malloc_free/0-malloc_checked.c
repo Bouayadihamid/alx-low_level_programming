09712a0edb36e1ea9813115413b4c17c88e84623
Hamid <Bouayadihamid@gmail.com>
2023-10-07 14:41:30 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *malloc_checked- is a function
+ *
+ *@b: unsigned integere
+ *
+ *Return: a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+
+ptr = malloc(b * sizeof(int));
+if (ptr == NULL)
+{
+	exit (98);
+}
+return (ptr);
+}
