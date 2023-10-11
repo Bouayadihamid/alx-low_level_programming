43d842788fe863aa3bdd5bcab4242adfa17c81a0
Hamid <Bouayadihamid@gmail.com>
2023-10-10 17:58:13 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog dtructure
+ *
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ *
+ *Descritption: just a lo
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
