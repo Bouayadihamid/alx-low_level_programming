1ad9b9427a0cac330d075b19c5beaa1cd01d3117
Hamid <Bouayadihamid@gmail.com>
2023-10-11 13:25:55 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,14 +14,15 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+	dog_t *newDog;
 
+	newDog = (dog_t *)malloc(sizeof(dog_t));
 	if (newDog == NULL)
 	{
 		return (NULL);
 	}
-	newDog->name = strdup(name);
+	newDog->name = name;
 	newDog->age = age;
-	newDog->owner = strdup(owner);
+	newDog->owner = owner;
 return (newDog);
 }
