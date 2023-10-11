3a913490a6e504ecd2d56b44ed27befdc32eb225
Hamid <Bouayadihamid@gmail.com>
2023-10-11 13:33:01 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,8 +21,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
+	if (name == NULL || owner == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
 	newDog->name = name;
 	newDog->age = age;
 	newDog->owner = owner;
-return (newDog);
+	return (newDog);
 }
