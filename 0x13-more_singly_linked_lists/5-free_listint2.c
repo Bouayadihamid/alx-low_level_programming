63f90503b1d2b7f0772d6077ecd39954da46d5bb
Hamid <Bouayadihamid@gmail.com>
2023-10-24 17:50:33 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list
+ * @head: ptr
+ * 
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *tmp;
+
+	if (!head)
+		return ();
+	node = *head;
+	while (node)
+	{
+		tmp = node;
+		node = node->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
