df35fecfc8fba12749af716f9e10297db4acf508
Hamid <Bouayadihamid@gmail.com>
2023-12-07 17:32:23 -0800

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistinct - prints doubly linked list
+ * @h: the head node address
+ *
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node_count  = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
