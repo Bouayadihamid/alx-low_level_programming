ab60c5e64a03d8d780a63ba943aecd85144b3807
Hamid <Bouayadihamid@gmail.com>
2023-12-07 17:44:53 -0800

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistinct- prints doubly linked list
+ * print_dlistint- prints doubly linked list
  * @h: the head node address
  *
  * Return: size of list
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints the length of a doubly linked list
+ * @h: the head node address
+ *
+ * Return: size of list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t node_count  = 0;
+
+        while (h)
+        {
+                h = h->next;
+                node_count++;
+        }
+        return (node_count);
+}
