e435d20afdf3fc8f26ccb1dd93cf8b74565e92a8
Hamid <Bouayadihamid@gmail.com>
2023-12-07 17:47:33 -0800

    Print the length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,16 +5,16 @@
  * @h: the head node address
  *
  * Return: size of list
- */
+ * */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t node_count  = 0;
+	size_t node_count  = 0;
 
-        while (h)
-        {
-                h = h->next;
-                node_count++;
-        }
-        return (node_count);
+	while (h)
+	{
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
 }
