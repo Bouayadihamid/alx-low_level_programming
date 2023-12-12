9ae8d059882ca3114a6a9178cfcb24e74f03b10b
Hamid <Bouayadihamid@gmail.com>
2023-12-11 17:30:46 -0800

    All

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Bouayadihamid/alx-low_level_programming.git/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/Bouayadihamid/alx-low_level_programming.git/tree/79b8b822500b32858c36ff9b5b5f2cabd8868b72/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
